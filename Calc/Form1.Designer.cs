1ce83176f1d2b1ee992a9929d5b22ebc5c8ad964
Stas <garrixdit@gmail.com>
2024-03-27 09:50:36 +0200

    Add project files.

---
+++ Calc/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace Calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn = new Button();
+            equal = new Button();
+            numberButton = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            comma = new Button();
+            clear = new Button();
+            specOperations = new Button();
+            button14 = new Button();
+            tb = new TextBox();
+            operation = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            revert = new Button();
+            SuspendLayout();
+            // 
+            // btn
+            // 
+            btn.FlatStyle = FlatStyle.Flat;
+            btn.Location = new Point(107, 436);
+            btn.Name = "btn";
+            btn.Size = new Size(85, 79);
+            btn.TabIndex = 0;
+            btn.Text = "0";
+            btn.UseVisualStyleBackColor = true;
+            btn.Click += numberButton_Click;
+            // 
+            // equal
+            // 
+            equal.FlatStyle = FlatStyle.Flat;
+            equal.Location = new Point(198, 436);
+            equal.Name = "equal";
+            equal.Size = new Size(85, 79);
+            equal.TabIndex = 1;
+            equal.Text = "=";
+            equal.UseVisualStyleBackColor = true;
+            equal.Click += equal_Click;
+            // 
+            // numberButton
+            // 
+            numberButton.FlatStyle = FlatStyle.Flat;
+            numberButton.Location = new Point(16, 351);
+            numberButton.Name = "numberButton";
+            numberButton.Size = new Size(85, 79);
+            numberButton.TabIndex = 3;
+            numberButton.Text = "1";
+            numberButton.UseVisualStyleBackColor = true;
+            numberButton.Click += numberButton_Click;
+            // 
+            // button4
+            // 
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Location = new Point(15, 266);
+            button4.Name = "button4";
+            button4.Size = new Size(85, 79);
+            button4.TabIndex = 4;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += numberButton_Click;
+            // 
+            // button5
+            // 
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Location = new Point(16, 181);
+            button5.Name = "button5";
+            button5.Size = new Size(85, 79);
+            button5.TabIndex = 5;
+            button5.Text = "7";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += numberButton_Click;
+            // 
+            // button6
+            // 
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Location = new Point(107, 351);
+            button6.Name = "button6";
+            button6.Size = new Size(85, 79);
+            button6.TabIndex = 6;
+            button6.Text = "2";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += numberButton_Click;
+            // 
+            // button7
+            // 
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Location = new Point(107, 266);
+            button7.Name = "button7";
+            button7.Size = new Size(85, 79);
+            button7.TabIndex = 7;
+            button7.Text = "5";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += numberButton_Click;
+            // 
+            // button8
+            // 
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Location = new Point(107, 181);
+            button8.Name = "button8";
+            button8.Size = new Size(85, 79);
+            button8.TabIndex = 8;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += numberButton_Click;
+            // 
+            // button9
+            // 
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.Location = new Point(198, 181);
+            button9.Name = "button9";
+            button9.Size = new Size(85, 79);
+            button9.TabIndex = 9;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += numberButton_Click;
+            // 
+            // button10
+            // 
+            button10.FlatStyle = FlatStyle.Flat;
+            button10.Location = new Point(198, 266);
+            button10.Name = "button10";
+            button10.Size = new Size(85, 79);
+            button10.TabIndex = 10;
+            button10.Text = "6";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += numberButton_Click;
+            // 
+            // button11
+            // 
+            button11.FlatStyle = FlatStyle.Flat;
+            button11.Location = new Point(198, 351);
+            button11.Name = "button11";
+            button11.Size = new Size(85, 79);
+            button11.TabIndex = 11;
+            button11.Text = "3";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += numberButton_Click;
+            // 
+            // comma
+            // 
+            comma.FlatStyle = FlatStyle.Flat;
+            comma.Location = new Point(16, 436);
+            comma.Name = "comma";
+            comma.Size = new Size(85, 79);
+            comma.TabIndex = 12;
+            comma.Text = ",";
+            comma.UseVisualStyleBackColor = true;
+            comma.Click += comma_Click;
+            // 
+            // clear
+            // 
+            clear.FlatStyle = FlatStyle.Flat;
+            clear.Location = new Point(16, 123);
+            clear.Name = "clear";
+            clear.Size = new Size(85, 52);
+            clear.TabIndex = 13;
+            clear.Text = "C";
+            clear.UseVisualStyleBackColor = true;
+            clear.Click += clear_Click;
+            // 
+            // specOperations
+            // 
+            specOperations.FlatStyle = FlatStyle.Flat;
+            specOperations.Location = new Point(107, 123);
+            specOperations.Name = "specOperations";
+            specOperations.Size = new Size(85, 52);
+            specOperations.TabIndex = 14;
+            specOperations.Text = "x²";
+            specOperations.UseVisualStyleBackColor = true;
+            specOperations.Click += specOperations_Click;
+            // 
+            // button14
+            // 
+            button14.FlatStyle = FlatStyle.Flat;
+            button14.Location = new Point(198, 123);
+            button14.Name = "button14";
+            button14.Size = new Size(85, 52);
+            button14.TabIndex = 15;
+            button14.Text = "√";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += specOperations_Click;
+            // 
+            // tb
+            // 
+            tb.Font = new Font("Segoe UI", 32F, FontStyle.Regular, GraphicsUnit.Point);
+            tb.Location = new Point(15, 53);
+            tb.Name = "tb";
+            tb.Size = new Size(359, 64);
+            tb.TabIndex = 16;
+            // 
+            // operation
+            // 
+            operation.FlatStyle = FlatStyle.Flat;
+            operation.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            operation.Location = new Point(289, 436);
+            operation.Name = "operation";
+            operation.Size = new Size(85, 79);
+            operation.TabIndex = 17;
+            operation.Text = "-";
+            operation.UseVisualStyleBackColor = true;
+            operation.Click += operation_Click;
+            // 
+            // button15
+            // 
+            button15.FlatStyle = FlatStyle.Flat;
+            button15.Location = new Point(289, 351);
+            button15.Name = "button15";
+            button15.Size = new Size(85, 79);
+            button15.TabIndex = 18;
+            button15.Text = "+";
+            button15.UseVisualStyleBackColor = true;
+            button15.Click += operation_Click;
+            // 
+            // button16
+            // 
+            button16.FlatStyle = FlatStyle.Flat;
+            button16.Location = new Point(289, 266);
+            button16.Name = "button16";
+            button16.Size = new Size(85, 79);
+            button16.TabIndex = 19;
+            button16.Text = "*";
+            button16.UseVisualStyleBackColor = true;
+            button16.Click += operation_Click;
+            // 
+            // button17
+            // 
+            button17.FlatStyle = FlatStyle.Flat;
+            button17.Location = new Point(289, 181);
+            button17.Name = "button17";
+            button17.Size = new Size(85, 79);
+            button17.TabIndex = 20;
+            button17.Text = "/";
+            button17.UseVisualStyleBackColor = true;
+            button17.Click += operation_Click;
+            // 
+            // revert
+            // 
+            revert.FlatStyle = FlatStyle.Flat;
+            revert.Location = new Point(289, 123);
+            revert.Name = "revert";
+            revert.Size = new Size(85, 52);
+            revert.TabIndex = 21;
+            revert.Text = "+-";
+            revert.UseVisualStyleBackColor = true;
+            revert.Click += revert_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(391, 527);
+            Controls.Add(revert);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(operation);
+            Controls.Add(tb);
+            Controls.Add(button14);
+            Controls.Add(specOperations);
+            Controls.Add(clear);
+            Controls.Add(comma);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(numberButton);
+            Controls.Add(equal);
+            Controls.Add(btn);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btn;
+        private Button equal;
+        private Button numberButton;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button comma;
+        private Button clear;
+        private Button specOperations;
+        private Button button14;
+        private TextBox tb;
+        private Button operation;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button revert;
+    }
+}
---
+++ Calc/Form1.cs
@@ -0,0 +1,134 @@
+﻿namespace Calc
+{
+    public partial class Form1 : Form
+    {
+        private double currentValue = 0;
+        private string currentOperator = "";
+        private bool isNewNumber = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numberButton_Click(object sender, EventArgs e)
+        {
+            if (isNewNumber)
+            {
+                tb.Text = "";
+                isNewNumber = false;
+            }
+
+            Button button = (Button)sender;
+            tb.Text += button.Text;
+        }
+
+        private void operation_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string newOperator = button.Text;
+
+            if (!isNewNumber)
+            {
+                Calculate();
+                currentOperator = newOperator;
+                isNewNumber = true;
+            }
+            else
+            {
+                currentOperator = newOperator;
+            }
+        }
+
+        private void equal_Click(object sender, EventArgs e)
+        {
+            Calculate();
+            currentOperator = "";
+            isNewNumber = true;
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            currentValue = 0;
+            currentOperator = "";
+            tb.Text = "0";
+            isNewNumber = true;
+        }
+
+        private void Calculate()
+        {
+            double newValue = double.Parse(tb.Text);
+
+            switch (currentOperator)
+            {
+                case "+":
+                    currentValue += newValue;
+                    break;
+                case "-":
+                    currentValue -= newValue;
+                    break;
+                case "*":
+                    currentValue *= newValue;
+                    break;
+                case "/":
+                    currentValue /= newValue;
+                    break;
+                case "√":
+                    currentValue = Math.Sqrt(currentValue);
+                    break;
+                case "x²":
+                    currentValue *= currentValue;
+                    break;
+                default:
+                    currentValue = newValue;
+                    break;
+            }
+
+            tb.Text = currentValue.ToString();
+        }
+
+        private void specOperations_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string newOperator = button.Text;
+            if (!isNewNumber)
+            {
+                currentOperator = newOperator;
+                isNewNumber = true;
+            }
+            else
+            {
+                currentOperator = newOperator;
+            }
+
+            double newValue = double.Parse(tb.Text);
+
+            switch (currentOperator)
+            {
+                case "√":
+                    newValue = Math.Sqrt(newValue);
+                    break;
+                case "x²":
+                    newValue *= newValue;
+                    break;
+                default:
+                    newValue = -1;
+                    break;
+            }
+            currentValue = newValue;
+            tb.Text = currentValue.ToString();
+        }
+
+        private void revert_Click(object sender, EventArgs e)
+        {
+            double value = double.Parse(tb.Text);
+            value -= value * 2;
+            tb.Text = value.ToString();
+        }
+
+        private void comma_Click(object sender, EventArgs e)
+        {
+            tb.Text += ",";
+        }
+    }
+}
